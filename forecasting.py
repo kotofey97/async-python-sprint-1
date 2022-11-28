f34844e27c3ae0818b69fd76e8432f4f57ff3acf
Olya <olya_sitnikova@mail.ru>
2022-11-28 23:55:20 +0300

    daily data

--- forecasting.py
+++ forecasting.py
@@ -1,7 +1,7 @@
 import logging
 import threading
 import subprocess
-import multiprocessing
+from multiprocessing import cpu_count, Pool, Manager
 from concurrent.futures import ThreadPoolExecutor
 
 from api_client import YandexWeatherAPI
@@ -15,6 +15,8 @@ from utils import CITIES
 
 logger = logging.getLogger(__name__)
 
+TASK_TIMEOUT = 30
+
 def forecast_weather():
     """
     Анализ погодных условий по городам
@@ -23,11 +25,26 @@ def forecast_weather():
     logger.debug("Thread workers count %s", count_workers)
     with ThreadPoolExecutor(max_workers=count_workers) as executor:
         futures = executor.map(DataFetchingTask.fetch, CITIES.keys())
-        data = list(futures)
+        data = list(futures)[:1]
+
+    manager = Manager()
+    # queue = multiprocessing.Queue()
+    queue = manager.Queue()
+    calculation = DataCalculationTask(queue=queue, hours_to_start=9, hours_to_finish=19)
+
+    workers_cpu = cpu_count() - 1
+
+    with Pool(processes=workers_cpu) as pool:
+        calculation_tasks = pool.map_async(calculation.calculate, data)
+
+        calculation_tasks.wait(TASK_TIMEOUT)
+    print('dddddd', calculation_tasks.ready())
+
 
     # city_name = "MOSCOW"
     # data = DataFetchingTask.fetch(city_name)
-    print("HHHH", data[0].forecasts)
+    # print("HHHH", data[0].forecasts)
+    # print("HHHH", len(data[0].forecasts.forecasts))
     # print("HHHH", data['forecasts'])
 
 
--- tasks.py
+++ tasks.py
@@ -1,17 +1,61 @@
 from api_client import YandexWeatherAPI
 from data_objs import CityModel
+from pandas import DataFrame as df
+import operator
 
 class DataFetchingTask:
     """Получение данных через API."""
     @staticmethod
     def fetch(city: str):
         result = YandexWeatherAPI().get_forecasting(city)
-        # print(result)
         return CityModel(city=city, forecasts=result)
 
 
 class DataCalculationTask:
-    pass
+
+    def __init__(self, queue, hours_to_start, hours_to_finish) -> None:
+        self.queue = queue
+        self.hours_to_start = hours_to_start
+        self.hours_to_finish = hours_to_finish
+    
+    def calculate(self, data):
+        forecasts = data.forecasts.dict()
+        city_name = data.city
+        # print('calculate', forecasts)
+        # print('city_name', city_name)
+        self.daily_stat(forecasts)
+
+    def daily_stat(self, forecasts):
+        daily_averages = df(
+            columns=["day_temp", "clear"],
+        ).transpose()
+        print('GGG', daily_averages)
+        columns = ["hour", "condition", "temp"]
+        types = {"hour": "int32", "temp": "int32"}
+        
+
+        for day in forecasts["forecasts"]:
+            hours_day = df.from_records(
+                day["hours"],
+                columns=columns,
+            ).astype(types)
+            hours = hours_day.loc[(hours_day["hour"] >= self.hours_to_start) & (hours_day["hour"] < self.hours_to_finish)]
+            if not hours.empty:
+                avg_day_temp = hours["temp"].mean().round(2)
+                clearly_hours = hours.query("condition == 'clear' | condition == 'partly-cloudy' | condition == 'cloudy' | condition == 'overcast'").agg(
+                    ["count"]
+                )["condition"]["count"]
+            else:
+                avg_day_temp = None
+                clearly_hours = None
+            daily_averages.loc["day_temp", day["date"]] = avg_day_temp
+            daily_averages.loc["clear", day["date"]] = clearly_hours
+
+        print('HHHHHH', daily_averages)
+        return daily_averages
+
+
+
 
 
 class DataAggregationTask:
